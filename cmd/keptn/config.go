8a1ee2b891ade029198e3eeeae02b7dcdb81ee3f
Huseyin BABAL <huseyinbabal88@gmail.com>
2023-03-06 12:37:28 +0300

    initial plugin

---
+++ cmd/keptn/config.go
@@ -0,0 +1,33 @@
+package keptn
+
+import (
+	"fmt"
+
+	"github.com/kubeshop/botkube/pkg/api/source"
+	"github.com/kubeshop/botkube/pkg/pluginx"
+)
+
+// Config Keptn configuration
+type Config struct {
+	URL     string `yaml:"url,omitempty"`
+	Token   string `yaml:"token,omitempty"`
+	Project string `yaml:"project,omitempty"`
+	Service string `yaml:"service,omitempty"`
+}
+
+// Log logging configuration
+type Log struct {
+	Level string `yaml:"level"`
+}
+
+// MergeConfigs merges all input configuration.
+func MergeConfigs(configs []*source.Config) (Config, error) {
+	defaults := Config{}
+
+	var out Config
+	if err := pluginx.MergeSourceConfigsWithDefaults(defaults, configs, &out); err != nil {
+		return Config{}, fmt.Errorf("while merging configuration: %w", err)
+	}
+
+	return out, nil
+}
